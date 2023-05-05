6c1b59fafe304b1b85055ed00b4d949c4788ea59
Ososki01 <karisikgaming@gmail.com>
2023-05-05 16:31:57 +0300

    new change in add fxml.

--- src/main/java/com/example/ce216project/GUIadd.java
+++ src/main/java/com/example/ce216project/GUIadd.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class GUIadd implements Initializable {
@@ -28,6 +29,9 @@ public class GUIadd implements Initializable {
     @FXML
     public TextField addtext4;
 
+    @FXML
+    public Button saveButton;
+
     public String[]language={"Turkish","English","French","Italian","Swedish","German","Modern Greek"};
     public static String mainLanguage;
     public static Graph graph;
@@ -44,32 +48,45 @@ public class GUIadd implements Initializable {
         this.mainLanguage = mainLanguage;
         return mainLanguage;
     }
-    public void displayAddWordForm(ActionEvent event)throws IOException
-    {
+    public void displayAddWordForm(ActionEvent event)throws IOException {
         addtext1.setVisible(true);
         addtext2.setVisible(true);
         addtext3.setVisible(true);
         addtext4.setVisible(true);
+    }
 
-        if (getLanguage().equals("English"))
-        {
+    public void enterAddWorForm(ActionEvent event) throws  IOException {
+        if (getLanguage().equals("English")) {
             //graph.addWord(wordTextField.getText(), "English", "Turkish", addtext1.getText());
-            //
+            System.out.println(wordTextField.getText());
+            ArrayList<String>turkishtranslate = new ArrayList<>();
+            ArrayList<String>germantranslate = new ArrayList<>();
+            ArrayList<String>moderngtranslate = new ArrayList<>();
+            ArrayList<String>sweedishtranslate = new ArrayList<>();
+            turkishtranslate.add(addtext1.getText());
+            germantranslate.add(addtext2.getText());
+            moderngtranslate.add(addtext3.getText());
+            sweedishtranslate.add(addtext4.getText());
 
-
+            if (saveButton.isDisabled() == false) {
+                Dictionary.graph.addWord(wordTextField.getText(),"English","Turkish",turkishtranslate);
+                Dictionary.graph.addWord(wordTextField.getText(),"English","German",germantranslate);
+                Dictionary.graph.addWord(wordTextField.getText(),"English","Modern Greek",moderngtranslate);
+                Dictionary.graph.addWord(wordTextField.getText(),"English","Sweedish",sweedishtranslate);
+            }
         }
-        if (getLanguage().equals("Turkish"))
-        {}
-        if (getLanguage().equals("Swedish"))
-        {}
-        if (getLanguage().equals("Italian"))
-        {}
-        if (getLanguage().equals("Modern Greek"))
-        {}
-        if (getLanguage().equals("French"))
-        {}
-        if (getLanguage().equals("German"))
-        {}
+        if (getLanguage().equals("Turkish")) {
+        }
+        if (getLanguage().equals("Swedish")) {
+        }
+        if (getLanguage().equals("Italian")) {
+        }
+        if (getLanguage().equals("Modern Greek")) {
+        }
+        if (getLanguage().equals("French")) {
+        }
+        if (getLanguage().equals("German")) {
+        }
 
     }
 }
